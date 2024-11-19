40badade5358829edffb9e3aefc978e851e6d1c9
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-19 13:27:38 +0600

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,10 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  theme: {},
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["lofi"],
   },
-  plugins: [],
 } satisfies Config;
