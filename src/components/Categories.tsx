84627bcff7d378f1f3d473f22a6736b3b0cc7487
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-28 00:44:03 +0600

    update category

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -24,33 +24,42 @@ const Categories = ({ isHome }: { isHome?: boolean }) => {
     <section className="wrapper">
       <SectionTitle title="Categories" />
       <div className="grid-container">
-        {data.categories.map((category) => (
-          <Link
-            href={`/categories/${category._id}`}
-            key={category._id}
-            className={cn(
-              isHome ? "h-40" : "h-96",
-              "w-full relative flex justify-center items-center shadow rounded overflow-hidden"
-            )}
-          >
-            <div className="w-full h-full absolute top-0 left-0 bottom-0 right-0 z-[-2]">
-              <Image
-                src={categoryImages.get(category.name as string)!}
-                alt={category.name!}
-                width={1280}
-                height={720}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div
+        {data.categories
+          .slice()
+          .sort(
+            (a, b) =>
+              (b.products?.length as number) - (a.products?.length as number)
+          )
+          .slice(0, 11)
+          .map((category) => (
+            <Link
+              href={`/categories/${category._id}`}
+              key={category._id}
               className={cn(
-                isHome ? "bg-black/75" : "bg-black/50",
-                "absolute z-[-1] top-0 left-0 right-0 bottom-0 w-full h-full"
+                isHome ? "h-40" : "h-96",
+                "w-full relative flex justify-center items-center shadow rounded overflow-hidden"
               )}
-            ></div>
-            <h4 className="card-title text-neutral-content">{category.name}</h4>
-          </Link>
-        ))}
+            >
+              <div className="w-full h-full absolute top-0 left-0 bottom-0 right-0 z-[-2]">
+                <Image
+                  src={categoryImages.get(category.name as string)!}
+                  alt={category.name!}
+                  width={1280}
+                  height={720}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div
+                className={cn(
+                  isHome ? "bg-black/75" : "bg-black/50",
+                  "absolute z-[-1] top-0 left-0 right-0 bottom-0 w-full h-full"
+                )}
+              ></div>
+              <h4 className="card-title text-neutral-content">
+                {category.name}
+              </h4>
+            </Link>
+          ))}
       </div>
     </section>
   );
