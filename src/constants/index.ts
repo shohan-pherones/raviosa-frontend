06134ceb8231d31d41236ab4591f78ffd5292356
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-28 01:18:35 +0600

    update categories images

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -13,4 +13,37 @@ export const categoryImages = new Map([
   ["Vagina Care", "/images/vagina_care.jpg"],
   ["Hair Removal", "/images/hair_removal.jpg"],
   ["Breast Growth", "/images/breast_growth.jpg"],
+  [
+    "Pregnancy & Maternity Care",
+    "/images/gettyimages-1310443783-20241118200956081.jpg",
+  ],
+  ["Sensitive Skin Care", "/images/4d26899e-670a-4427-96ff-eb3da4f91aba.jpeg"],
+  ["Lips & Lip Care", "/images/3_8e9c7614-349b-4bb6-9534-2050b7f33354.jpg"],
+  ["Eye Care", "/images/blog-woman-heart-hands.jpg"],
+  [
+    "Anti-aging",
+    "/images/anti-aging-GettyImages-1478914219-44241f3408a34ee8ac99f0f82c8482c9.jpg",
+  ],
+  [
+    "Acne & Blemish Care",
+    "/images/acne-treatment-collection-free-blemish-control-mukti-organics-695917_800x.jpg",
+  ],
+  [
+    "Face Tanning & Bronzing",
+    "/images/S0a669cdd60aa4042a02ba4bd25d692130.jpg_640x640q90.jpg",
+  ],
+  ["Lip Plumping", "/images/Lip-Plumping-Pen-2024-04-25_HERO-min.jpg"],
+  [
+    "Bath & Shower Products",
+    "/images/young-woman-in-bubble-bath-laughs-and-pats-bubbles-on-body-and-face-video.jpg",
+  ],
+  ["Cellulite Treatment", "/images/shutterstock_1768941050-1.jpg"],
+  [
+    "Feminine Hygiene",
+    "/images/Are-Feminine-Hygiene-Products-Safe-for-Treating-Vaginal-Odor_.jpg",
+  ],
+  ["Luxury Cosmetics", "/images/Types-of-Luxury-Private-Label-Cosmetics-.jpg"],
+  ["Hair Color & Dye", "/images/1200px-Dyed-hair-purple.png"],
+  ["Spa & Relaxation", "/images/spa_and_relaxation.jpg"],
+  ["Makeup Removers", "/images/makeup_removers.jpg"],
 ]);
